7789ebdad3832505098545a246d251fd46131343
longzannn <luczanthien@gmail.com>
2024-02-29 21:44:21 +0700

    code login, register page

--- client/src/components/SubHeader/SubHeader.jsx
+++ client/src/components/SubHeader/SubHeader.jsx
@@ -1,8 +1,9 @@
 function SubHeader() {
   return (
-    <header className='h-[132px]'>
-      <div className='bg-[#fe0000] h-[88px] fixed top-0 left-0 right-0 z-20'>
-        <div className='max-w-[1200px] h-full m-auto flex items-center gap-8'>
+    <header className='h-[88px]'>
+      <div className='bg-[#ec2127] h-[88px] fixed top-0 left-0 right-0 z-20'>
+        <div className='h-full max-w-[95%] m-auto flex items-center justify-between gap-8'>
+          <div className='font-semibold text-white cursor-pointer hover:underline'>Trở về</div>
           <img
             className='cursor-pointer'
             src='https://www.nguyenkim.com/images/companies/_1/html/2017/T11/homepage/Logo_NK.svg'
--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,5 +1,29 @@
+import { ChevronRightIcon } from '@radix-ui/react-icons'
+import { Flex, TextField } from '@radix-ui/themes'
+
 function Register() {
-  return <div></div>
+  return (
+    <div className='bg-white py-16'>
+      <div className='max-w-[600px] form-shadow px-12 py-8 m-auto'>
+        <h2 className='uppercase text-lg font-medium text-center mb-7 cursor-pointer'>
+          <span className='text-[#ed3324] border-b-2 py-2 px-4 border-b-[#ed3324]'>Đăng ký</span>
+        </h2>
+        <form action=''>
+          <Flex direction='column' gap='4'>
+            <TextField.Input radius='small' size='3' placeholder='Họ và Tên' />
+            <TextField.Input radius='small' size='3' placeholder='Số điện thoại' />
+            <TextField.Input radius='small' size='3' placeholder='Email' />
+            <TextField.Input radius='small' size='3' placeholder='Mật khẩu' />
+            <TextField.Input radius='small' size='3' placeholder='Nhập lại mật khẩu' />
+          </Flex>
+          <button className='bg-[#ec2127] flex items-center justify-center w-full p-4 rounded-md hover:opacity-80 mt-8'>
+            <span className='text-white font-normal text-xl uppercase'>Đăng ký</span>
+            <ChevronRightIcon width={25} height={25} color='white' />
+          </button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Register
